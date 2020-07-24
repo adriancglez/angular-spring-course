b211670c1608612e98a667b3a004165d2fbc8290
Adrián González <39930633+adriancglez@users.noreply.github.com>
2020-07-23 21:19:45 -0500

    Revert "Se integra nuevo componente clientes para reforzar lo aprendido - resolve #16"

--- 01_Angular_Basics/a-clientes-app/src/app/app.module.ts
+++ 01_Angular_Basics/a-clientes-app/src/app/app.module.ts
@@ -5,15 +5,13 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DirectivaComponent } from './directiva/directiva.component';
-import { ClientesComponent } from './clientes/clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DirectivaComponent,
-    ClientesComponent
+    DirectivaComponent
   ],
   imports: [
     BrowserModule
