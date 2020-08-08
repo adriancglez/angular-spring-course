f7cf021c0a88b657d41fe2f6089037bafed5323d
Adrian Gonzalez <yo.robot.1990@gmail.com>
2020-08-08 00:14:06 -0500

    Validación de datos por URL en BackEnd y FrontEnd - resolve #31

--- 03_Angular-SpringREST/customers-app-backend/src/main/java/com/smoothiemx/customersapp/app/controllers/ClienteRestController.java
+++ 03_Angular-SpringREST/customers-app-backend/src/main/java/com/smoothiemx/customersapp/app/controllers/ClienteRestController.java
@@ -3,10 +3,15 @@ package com.smoothiemx.customersapp.app.controllers;
 import com.smoothiemx.customersapp.app.models.Cliente;
 import com.smoothiemx.customersapp.app.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -27,29 +32,130 @@ public class ClienteRestController {
     }
 
     @GetMapping("/clientes/{id}")
-    public Cliente show(@PathVariable("id") Long id) {
-        return this.iClienteService.findById(id);
+    public ResponseEntity<?> show(@PathVariable("id") String idx) {
+
+        Cliente cliente = null;
+        Map<String, Object> response = new HashMap<>();
+        long id;
+
+        try {
+            id = Long.parseLong(idx);
+            cliente = this.iClienteService.findById(id);
+
+            if(cliente == null) {
+                response.put("mensaje", "Error al consultar Cliente en el Sistema");
+                response.put("error", "El cliente con el id ".concat(Long.toString(id).concat( " no existe en el sistema")));
+
+                return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+            }
+        } catch (DataAccessException ex) {
+            response.put("mensaje", "Error al consultar Cliente en el Sistema");
+            response.put("error", Objects.requireNonNull(ex.getMessage()).concat(" ").concat(ex.getMostSpecificCause().getMessage()));
+
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (NumberFormatException ex) {
+            response.put("mensaje", "El id es inválido");
+            response.put("error", Objects.requireNonNull(ex.getMessage()).concat(" ").concat(ex.getLocalizedMessage()));
+
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
     }
 
     @PostMapping("/clientes")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Cliente create(@RequestBody Cliente cliente) {
-        return this.iClienteService.save(cliente);
+    public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+
+        Cliente nuevoCliente = null;
+        Map<String, Object> response = new HashMap<>();
+
+        try {
+            nuevoCliente = this.iClienteService.save(cliente);
+        } catch (DataAccessException ex) {
+            response.put("mensaje", "Error al guardar cliente");
+            response.put("error", Objects.requireNonNull(ex.getMessage()).concat(": ").concat(ex.getMostSpecificCause().getMessage()));
+
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        response.put("mensaje", "El cliente ha sido creado con éxito");
+        response.put("cliente", nuevoCliente);
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
     }
 
     @PutMapping("/clientes/{id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Cliente update(@RequestBody Cliente cliente, @PathVariable("id") Long id) {
-        Cliente clienteActual = this.iClienteService.findById(id);
-        clienteActual.setApellido(cliente.getApellido());
-        clienteActual.setNombre(cliente.getNombre());
-        clienteActual.setEmail(cliente.getEmail());
-        return this.iClienteService.save(clienteActual);
+    public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable("id") String idx) {
+
+
+        Cliente clienteActual = null;
+        Cliente clienteUpdated = null;
+        Map<String, Object> response = new HashMap<>();
+        long id;
+
+        try {
+            id = Long.parseLong(idx);
+            clienteActual = this.iClienteService.findById(id);
+
+            if (clienteActual == null) {
+                response.put("mensaje", "Error al actualizar cliente");
+                response.put("error", "El cliente con el id ".concat(Long.toString(id).concat(" no existe en el sistema")));
+
+                return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+            }
+
+            clienteUpdated = this.iClienteService.save(cliente);
+        } catch (DataAccessException ex) {
+            response.put("mensaje", "Error al actualizar cliente");
+            response.put("error", Objects.requireNonNull(ex.getMessage()).concat(" ").concat(ex.getLocalizedMessage()));
+
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        } catch (NumberFormatException ex) {
+            response.put("mensaje", "El id es inválido");
+            response.put("error", Objects.requireNonNull(ex.getMessage()).concat(" ").concat(ex.getLocalizedMessage()));
+
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        response.put("mensaje", "El cliente ha sido actualizado con éxito");
+        response.put("cliente", clienteUpdated);
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+
     }
 
     @DeleteMapping("/clientes/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable("id") Long id) {
-        this.iClienteService.delete(id);
+    public ResponseEntity<?> delete(@PathVariable("id") String idx) {
+
+        Map<String, Object> response = new HashMap<>();
+        long id;
+
+        try {
+            id = Long.parseLong(idx);
+            Cliente cliente = this.iClienteService.findById(id);
+
+            if (cliente == null) {
+                response.put("mensaje", "Error al eliminar cliente");
+                response.put("error", "El cliente con el id ".concat(Long.toString(id).concat( " no existe en el sistema")));
+
+                return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+            }
+
+            this.iClienteService.delete(id);
+        } catch (DataAccessException ex) {
+            response.put("mensaje", "Error al eliminar cliente");
+            response.put("error", Objects.requireNonNull(ex.getMessage()).concat(": ").concat(ex.getMostSpecificCause().getMessage()));
+
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (NumberFormatException ex) {
+            response.put("mensaje", "El id es inválido");
+            response.put("error", Objects.requireNonNull(ex.getMessage()).concat(" ").concat(ex.getLocalizedMessage()));
+
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+        }
+
+        response.put("mensaje", "El cliente ha sido eliminado con éxito");
+
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
     }
 }
