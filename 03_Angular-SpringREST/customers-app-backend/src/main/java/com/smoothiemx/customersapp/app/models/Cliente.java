e871811390725845371af4eb2e9ba3cac148048c
Adrian Gonzalez <yo.robot.1990@gmail.com>
2020-08-24 21:21:55 -0500

    Integración de validaciones en Front y Back

--- 03_Angular-SpringREST/customers-app-backend/src/main/java/com/smoothiemx/customersapp/app/models/Cliente.java
+++ 03_Angular-SpringREST/customers-app-backend/src/main/java/com/smoothiemx/customersapp/app/models/Cliente.java
@@ -1,6 +1,9 @@
 package com.smoothiemx.customersapp.app.models;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 import java.io.Serializable;
 import java.util.Date;
 
@@ -12,15 +15,20 @@ public class Cliente implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty
+    @Size(min = 4, max = 12)
     @Column(nullable = false)
     private String nombre;
 
+    @NotEmpty
+    @Size(min = 4)
     private String apellido;
 
     @Column(nullable = false, unique = true)
     private String email;
 
     @Column(name = "create_at")
+    @Email
     @Temporal(TemporalType.DATE)
     private Date createAt;
 
